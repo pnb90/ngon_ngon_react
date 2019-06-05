a7fd1be2ba84445d8350d36aa1397547fd0697d5
pnb90 <phuocbui90@gmail.com>
2019-06-05 16:38:15 -0500

    started a search box

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,23 +1,48 @@
-import React from 'react'
-import SearchIcon from '@material-ui/icons/Search';
+import React, { useState }from 'react'
+import axios from 'axios'
 import InputBase from '@material-ui/core/InputBase';
 import styled from 'styled-components'
-
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
 
 const Container = styled.div`
   background: white;
-  width: 400px;
+  width: 300px;
   height: 31px;
   box-sizing: border-box;
 `
 
+function handleSubmit(searchTerms) {
+  const backend_search = searchTerms
+  // console.log("I'm in the handleSubmit Function")
+  // console.log(backend_search)
+  axios.get("/api/restaurants/search", backend_search)
+    .then(response => console.log(response))
+}
+
 function SearchBox(){
+  const [searchTerms, setSearchTerms] = useState("")
+
+  const handleSearchTermsChanges = (event) => { 
+    event.preventDefault();
+    setSearchTerms(event.target.value); 
+    }
+
   return(
     <Container>
-      <SearchIcon />
       <InputBase 
-        placeholder = "Search..."
+        value = {searchTerms}
+        onChange = {handleSearchTermsChanges}
+        type = "text"
       />
+      <IconButton 
+        variant = "contained"
+        size = "small"
+        aria-label = "search"
+        onSubmit = { handleSubmit(searchTerms) }
+      >
+        <SearchIcon /> 
+      </IconButton>
     </Container>
   )
 }
